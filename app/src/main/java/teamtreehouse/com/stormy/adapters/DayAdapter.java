b29146e612d01c7f770779c75bfc9435897741e1
dchi <dchievo@yahoo.com>
2015-10-14 15:18:51 -0700

    Added ViewHolder

---
+++ app/src/main/java/teamtreehouse/com/stormy/adapters/DayAdapter.java
@@ -0,0 +1,66 @@
+package teamtreehouse.com.stormy.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import teamtreehouse.com.stormy.R;
+import teamtreehouse.com.stormy.weather.Day;
+
+/**
+ * Created by dchi on 10/14/2015.
+ */
+public class DayAdapter extends BaseAdapter
+{
+    private Context mContext;
+    private Day[] mDays;
+
+    public DayAdapter(Context context, Day[] day)
+    {
+        mContext = context;
+        mDays = days;
+    }
+
+    @Override
+    public int getCount() {
+        return mDays.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return mDays[position];
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+
+        if (convertView == null)
+        {  //brand new
+            convertView = LayoutInflater.from(mContext).inflate(R.id.daily_list_item1);
+            holder = new ViewHolder();
+            holder.iconImageView = (ImageView) convertView.findViewById(R.id.iconImageView);
+            holder.dayLabel = (TextView) convertView.findViewById(R.id.dayNameLabel);
+            holder.temperatureLabel = (TextView) convertView.findViewById(R.id.temperatureLabel);
+
+            convertView.setTag(holder);
+        }
+        return null;
+    }
+
+    private static class ViewHolder
+    {
+        ImageView iconImageView;
+        TextView temperatureLabel;
+        TextView dayLabel;
+    }
+}
